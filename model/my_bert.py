c8593c0e38b817b2b97c66c2cc8801bec71f23ce
sjsun <stanleysun233@163.com>
2024-11-08 02:52:07 +0800

    update framework

--- model/my_bert.py
+++ model/my_bert.py
@@ -101,7 +101,66 @@ class BertBiLSTMMegaCRF(AutoModelForTokenClassification):
         if labels is not None:
             # 计算 CRF 损失
             log_likelihood = self.crf(logits, labels, mask=attention_mask.byte())
-            return -log_likelihood  # 返回负对数似然作为损失
+            return -log_likelihood * 10  # 返回负对数似然作为损失
+        else:
+            # 解码预测标签
+            predictions = self.crf.decode(logits, mask=attention_mask.byte())
+            return predictions
+
+
+class BertMegaBiLSTMCRF(AutoModelForTokenClassification):
+    def __init__(self, config, lstm_hidden_size=256):
+        super().__init__(config)
+
+        # MegaLayer 层
+        self.mega = MegaLayer(
+            dim=config.hidden_size,
+            ema_heads=32,
+            attn_dim_qk=128,
+            attn_dim_value=256,
+            laplacian_attn_fn=False,
+        )
+
+        # BiLSTM 层
+        self.lstm = nn.LSTM(
+            input_size=config.hidden_size,
+            hidden_size=lstm_hidden_size,
+            num_layers=1,
+            bidirectional=True,
+            batch_first=True
+        )
+
+        self.dropout = nn.Dropout(0.5)
+        self.layer_norm = nn.LayerNorm(lstm_hidden_size * 2)
+
+        # 线性分类器
+        self.classifier = nn.Linear(lstm_hidden_size * 2, config.num_labels)
+
+        # CRF 层
+        self.crf = CRF(config.num_labels, batch_first=True)
+
+    def forward(self, input_ids=None, attention_mask=None, labels=None, **kwargs):
+        # 获取 BERT 的输出
+        outputs = super().forward(input_ids=input_ids, attention_mask=attention_mask, labels=None, **kwargs)
+        sequence_output = outputs.last_hidden_state  # (batch_size, seq_length, hidden_size)
+        # sequence_output = self.dropout(sequence_output)
+
+        # 第一残差块：MegaLayer 层 + 残差
+        mega_output = self.mega(sequence_output)
+
+        # 第二残差块：BiLSTM 层 + 残差
+        lstm_output, _ = self.lstm(mega_output)  # (batch_size, seq_length, lstm_hidden_size * 2)
+        lstm_output = lstm_output + mega_output  # 将 MegaLayer 输出添加到 LSTM 输出
+        lstm_output = self.layer_norm(lstm_output)
+
+        # 第三残差块：分类器 + 残差
+        logits = self.classifier(lstm_output)
+
+        # 计算损失或解码预测
+        if labels is not None:
+            # 计算 CRF 损失
+            log_likelihood = self.crf(logits, labels, mask=attention_mask.byte())
+            return -log_likelihood * 10  # 返回负对数似然作为损失
         else:
             # 解码预测标签
             predictions = self.crf.decode(logits, mask=attention_mask.byte())
