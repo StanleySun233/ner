48e21f56e8e3a77cfbed9897c67e6d99541514bb
sjsun <stanleysun233@163.com>
2024-11-07 18:49:57 +0800

    update

--- model/my_bert.py
+++ model/my_bert.py
@@ -5,6 +5,7 @@ from torch import nn
 from transformers import MambaPreTrainedModel
 from transformers.modeling_outputs import TokenClassifierOutput
 from transformers import BertPreTrainedModel
+from mega_pytorch import MegaLayer
 
 import dst
 
@@ -49,6 +50,32 @@ class BertBiLSTMCRF(AutoModelForTokenClassification):
         # 添加 CRF 层
         self.crf = CRF(config.num_labels, batch_first=True)
 
+class BertBiLSTMMegaCRF(AutoModelForTokenClassification):
+    def __init__(self, config, lstm_hidden_size=256):
+        super().__init__(config)
+
+        # 添加 BiLSTM 层，输入大小为 BERT 的隐藏层大小，输出大小为自定义的 lstm_hidden_size
+        self.lstm = nn.LSTM(
+            input_size=config.hidden_size,
+            hidden_size=lstm_hidden_size,
+            num_layers=1,
+            bidirectional=True,
+            batch_first=True
+        )
+        self.mega = MegaLayer(
+            dim=lstm_hidden_size,  # model dimensions
+            ema_heads=16,  # number of EMA heads
+            attn_dim_qk=64,  # dimension of queries / keys in attention
+            attn_dim_value=256,  # dimension of values in attention
+            laplacian_attn_fn=False,  # whether to use softmax (false) or laplacian attention activation fn (true)
+        )
+
+        # 更新线性层的输入大小以适应 BiLSTM 的输出
+        self.classifier = nn.Linear(lstm_hidden_size * 2, config.num_labels)
+
+        # 添加 CRF 层
+        self.crf = CRF(config.num_labels, batch_first=True)
+
     def forward(self, input_ids=None, attention_mask=None, labels=None, **kwargs):
         # 获取 BERT 的输出
         outputs = super().forward(input_ids=input_ids, attention_mask=attention_mask, labels=None, **kwargs)
@@ -56,9 +83,9 @@ class BertBiLSTMCRF(AutoModelForTokenClassification):
 
         # 通过 BiLSTM 层
         lstm_output, _ = self.lstm(sequence_output)  # (batch_size, seq_length, lstm_hidden_size * 2)
-
+        mega_output = self.mega(lstm_output)
         # 通过线性分类器，将 lstm_output 的输出映射到标签空间
-        logits = self.classifier(lstm_output)  # (batch_size, seq_length, num_labels)
+        logits = self.classifier(mega_output)  # (batch_size, seq_length, num_labels)
 
         # 计算损失或解码预测
         if labels is not None:
